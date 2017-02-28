a8f424fac6b30ab7b6132559074ce1ff4b57bae9
amalshehu <amalshehu@gmail.com>
2017-02-28 19:30:48 +0530

    Angular 4 animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('collapsed, void',
+        style({ height: '0px', color: 'maroon', borderColor: 'maroon' })),
+      state('expanded',
+        style({ height: '*', borderColor: 'green', color: 'green' })),
+      transition('collapsed <=> expanded', [
+        animate(500, style({ height: '250px' })),
+        animate(500)
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app works!';
+  stateExpression: string;
+  constructor() {
+    this.collapse();
+  }
+  expand() {
+    this.stateExpression = 'expanded';
+  }
+  collapse() {
+    this.stateExpression = 'collapsed';
+  }
 }
